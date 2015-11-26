8663db63869d01b7ac0646bebdcc240d254272f1
Marín Salinas <marinssalinas@gmail.com>
2015-11-26 17:23:31 +0100

    Conn

--- conn.php
+++ conn.php
@@ -5,5 +5,5 @@
  * Date: 11/26/15
  * Time: 4:22 PM
  */
-$con = mysql_connect('localhost','chat','chatint');
+$con = mysql_connect('localhost:3306','chat','chatint');
 mysql_select_db('chatinteligente+',$con);
